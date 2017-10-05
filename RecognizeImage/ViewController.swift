fb31bbaa0d909303fc2ef1bb474ad79c49818a7e
ochi <mune.ochi@gmail.com>
2017-10-05 16:52:57 +0900

    Initial Commit

---
+++ RecognizeImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecognizeImage
+//
+//  Created by nijibox on 2017/10/05.
+//  Copyright © 2017年 recruit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
